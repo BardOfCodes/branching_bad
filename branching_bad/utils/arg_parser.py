a3116d549311c4908ad295c7da4651dc1d3f40f9
bardofcodes <adityaganeshan@gmail.com>
2023-04-20 15:58:25 -0400

    Added machine spec

--- branching_bad/utils/arg_parser.py
+++ branching_bad/utils/arg_parser.py
@@ -3,5 +3,4 @@ import argparse
 arg_parser = argparse.ArgumentParser(description="singular parser")
 arg_parser.add_argument('--config-file', type=str,
                         default="configs/config.yml")
-arg_parser.add_argument('--machine', type=str, default="MIKOSHI")
 arg_parser.add_argument('--debug', type=bool, default=False)
--- configs/ablations/pretrain.py
+++ configs/ablations/pretrain.py
@@ -3,15 +3,19 @@ from wacky import CfgNode as CN,  CfgNodeFactory as CNF
 from configs.subconfigs.domain import CSG2D
 from configs.subconfigs.model import MODEL
 
-MACHINE_DIR = "/media/aditya/DATA/projects/branching_bad"
-DATA_PATH = "/home/aditya/data/synthetic_data/FCSG2D_data"
 
 
 class PretrainConfFactory(CNF):
 
-    def __init__(self, name="PretrainClear"):
+    def __init__(self, name="PretrainClear", machine="local"):
         super(PretrainConfFactory, self).__init__()
 
+        if machine == "local": 
+            MACHINE_DIR = "/media/aditya/DATA/projects/branching_bad"
+            DATA_PATH = "/home/aditya/data/synthetic_data/FCSG2D_data"
+        else:
+            MACHINE_DIR = "/users/aganesh8/data/aganesh8/projects/branching_bad"
+            DATA_PATH = "/users/aganesh8/data/aganesh8/data/synthetic_data/FCSG2D_data"
         canvas_res = 64
         input_quant = 33
         # Create Config object:
